ebc060faee7d8b9a369bc9e0c892caf4f15e63f3
retropikzel <retropikzel@iki.fi>
2025-12-05 12:46:51 +0200

    Clean up the report

--- stats.scm
+++ stats.scm
@@ -65,7 +65,8 @@
 (define *group-results* '())
 (define *tests* (make-hash-table))
 
-(define (symbol-downcase s) (string->symbol (string-downcase (symbol->string s))))
+(define (symbol-downcase s)
+  (string->symbol (string-downcase (symbol->string s))))
 
 (define-syntax push
   (syntax-rules ()
@@ -74,22 +75,28 @@
 
 (define (parse-data)
   (let ((data (read-data)))
-    (for-each (lambda (d)
-                (let* ((impl (first d))
-                       (version (second d))
-                       (scheme (string->symbol (string-append impl "-" version)))
-                       (group (string->symbol (third d)))
-                       (result (symbol-downcase (string->symbol (fourth d))))
-                       (name (fifth d))
-                       (comments (apply string-append (list-tail d 5))))
-                  (nested-hash-table/put! *data* result group name comments scheme)
-                  (unless (memq scheme *schemes*)
-                    (push scheme *schemes*))
-                  (unless (memq group *groups*)
-                    (push group *groups*))
-                  (unless (member name (hash-table-ref/default *tests* group '()))
-                    (hash-table-set! *tests* group (cons name (hash-table-ref/default *tests* group '()))))))
-              data)))
+    (for-each
+      (lambda (d)
+        (let* ((impl (first d))
+               (version (second d))
+               (scheme (string->symbol (string-append impl "-" version)))
+               (group (string->symbol (third d)))
+               (result (symbol-downcase (string->symbol (fourth d))))
+               (name (fifth d))
+               (comments (apply string-append (list-tail d 5))))
+          (nested-hash-table/put! *data* result group name comments scheme)
+          (unless (memq scheme *schemes*)
+            (push scheme *schemes*))
+          (unless (memq group *groups*)
+            (push group *groups*))
+          (unless (member name (hash-table-ref/default *tests* group '()))
+            (hash-table-set! *tests*
+                             group
+                             (cons name
+                                   (hash-table-ref/default *tests*
+                                                           group
+                                                           '()))))))
+      data)))
 
 (define (get-res group test)
   (hash-table->alist (nested-hash-table/get *data* 'UNKNOWN group test)))
@@ -104,27 +111,69 @@
 (define *console* (current-output-port))
 
 (define (group-stats data group scheme)
-;;  (format *console* "group-stats ~a ~a ~a" data group scheme) (flush-output)
+  ;; (format *console* "group-stats ~a ~a ~a" data group scheme) (flush-output)
   (let ((gr (nested-hash-table/get data #f group))
         (pos 0)
         (neg 0))
-    (hash-table-walk gr
-                         (lambda (k function)
-                           (hash-table-walk function
-                                                (lambda (k test)
-                                                  (let ((r (hash-table-ref/default test scheme #f)))
-                                                    (if r
-                                                        (case r
-                                                          ((ok) (set! pos (+ pos 1)))
-                                                          ((error) (set! neg (+ neg 1)))
-                                                          (else (error "unknown result" r)))
-                                                        (format *console* "ERROR: no result for ~a ~a ~a~%" data group scheme)
-                                                        ))))))
+    (hash-table-walk
+      gr
+      (lambda (k function)
+        (hash-table-walk function
+                         (lambda (k test)
+                           (let ((r (hash-table-ref/default test scheme #f)))
+                             (if r
+                               (case r
+                                 ((ok) (set! pos (+ pos 1)))
+                                 ((error) (set! neg (+ neg 1)))
+                                 (else (error "unknown result" r)))
+                               (format *console*
+                                       "ERROR: no result for ~a ~a ~a~%"
+                                       data
+                                       group
+                                       scheme)))))))
     (format #f "~a ~a"
-            (if (zero? pos) "" (format #f "<small class=\"ok\">~a</small>✓" pos))
-            (if (zero? neg) "" (format #f "<small class=\"error\">~a</small>×" neg)))))
+            (if (zero? pos) "" (format #f "<small>~a</small>✓" pos))
+            (if (zero? neg) "" (format #f "<small>~a</small>×" neg)))))
+
+(define format-group-row
+  (lambda args
+    (format #t "<tr>")
+    (for-each (lambda (item) (format #t "<th><b>~a</b></th>" item)) args)
+    (format #t "</tr>")))
+
+(define format-row
+  (lambda args
+    (format #t "<tr>")
+    (for-each (lambda (item) (format #t "<td>~a</td>" item)) args)
+    (format #t "</tr>")))
+
+(define (format-thead scheme-list)
+  (format #t "<table border=1><thead><tr>")
+  (for-each
+    (lambda (scheme)
+      (if (symbol? scheme)
+        (let* ((parts (string-split (symbol->string scheme) #\-))
+               (name (car parts))
+               (version (cadr parts)))
+          (format #t
+                  "<th>~a<br/><small>~a</small></th>"
+                  name
+                  version))
+        (format #t "<th>~a</th>" scheme)))
+    (cons "" scheme-list))
+  (format #t "</tr></thead>"))
+
+(define (count-result r scheme)
+  (map
+    (lambda (res)
+      (if (eq? 'error (hash-table-ref/default (cdr res) scheme 'error))
+        0
+        1))
+    r))
+
 (define (format-stats)
-  (with-output-to-file "index.html"
+  (with-output-to-file
+    "index.html"
     (lambda ()
       (set! *data* (make-hash-table))
       (set! *schemes* '())
@@ -133,61 +182,39 @@
       (set! *tests* (make-hash-table))
       (parse-data)
       (let ((scheme-list (sort *schemes* (lambda (a b) (symbol<? a b)))))
-        (format #t "<html><head><meta charset=\"utf-8\"/></head><body>~%")
-        (format #t "<table border=1><thead><tr>")
-        (for-each (lambda (scheme)
-                    (if (symbol? scheme)
-                        (let* ((parts (string-split (symbol->string scheme) #\-))
-                               (name (car parts))
-                               (version (cadr parts)))
-                          (format #t "<th>~a<br/><small>~a</small></th>" name version))
-                        (format #t "<th>~a</th>" scheme)))
-                  (cons "" scheme-list))
-        (format #t "</tr></thead><tbody>")
-        (for-each (lambda (group)
-                    (format #t "<tr><td>~a</td>" (symbol->string group))
-                    (for-each (lambda (scheme)
-                                (format #t "<td>~a</td>" (group-stats *data* group scheme)))
-                              scheme-list)
-                    (format #t "</tr>~%")
-                    ;; (for-each (lambda (scheme)
-                    ;;             (let ((res (nested-hash-table/get *group-results* 0 group scheme)))
-                    ;;               (format #t "<td>~a/~a</td>~%" res (length (hash-table/get *tests* group '() )))))
-                    ;;           (sort *schemes* (lambda (a b) (symbol<? a b)))
-                    (format #t "</tr>~%")
-                    (for-each (lambda (test)
-                                (let ((r (get-res group test))
-                                      (id (next-id)))
-                                  (format #t "<tr><td>~a" test)
-                                  #;(for-each (lambda (res)
-                                              (format #t
-                                                      "<div>~a~%"
-                                                      (if (string=? "" (car res)) test (car res)))
-                                              (for-each (lambda (scheme)
-                                                          (let ((x (hash-table-ref/default (cdr res) scheme 'unknown)))
-                                                            (format #t "~a"
-                                                                    ;(if (eq? 'ok x) "ok" "error")
-                                                                    (if (eq? x 'ok) "✓" "×"))))
-                                                        (sort *schemes* (lambda (a b) (symbol<? a b))))
-                                              (format #t "</div>"))
-                                            (sort r (lambda (a b) (string<? (car a) (car b)))))
-                                  (format #t "</td>~%")
-                                  (for-each (lambda (scheme)
-                                              (let* ((x (map (lambda (res)
-                                                               (if (eq? 'error (hash-table-ref/default (cdr res) scheme 'error)) 0 1))
-                                                             r))
-                                                     (ok (apply + x))
-                                                     (total (length x)))
-                                                (format #t "<td>~a ~a</td>~%"
-                                                        (if (= ok total) "✓" (if (= 0 ok) "×" "◑"))
-                                                        (format #f "~a/~a" ok total))))
-                                            (sort *schemes* (lambda (a b) (symbol<? a b))))
-                                  (format #t "</tr>~%")
-                                  ))
-                              (sort (hash-table-ref/default *tests* group '())
-                                    (lambda (a b) (string<? a b)))))
-                  (sort *groups*
-                        (lambda (a b) (symbol<? a b))))
+        (format #t "<html><head><meta charset=\"utf-8\"/></head><body>")
+        (format-thead scheme-list)
+        (format #t "<tbody>")
+        (for-each
+          (lambda (group)
+            (apply format-group-row
+                   (cons (symbol->string group)
+                         (map (lambda (scheme)
+                                (group-stats *data* group scheme))
+                              scheme-list)))
+            (for-each
+              (lambda (test)
+                (let ((r (get-res group test))
+                      (id (next-id)))
+                  (apply format-row
+                         `(,test
+                            ,@(map
+                                (lambda (scheme)
+                                  (let* ((x (count-result r scheme))
+                                         (ok (apply + x))
+                                         (total (length x)))
+                                    (format "~a/~a ~a"
+                                            ok
+                                            total
+                                            (if (= ok total)
+                                              "✓"
+                                              (if (= 0 ok) "×" "◑")))))
+                                (sort *schemes*
+                                      (lambda (a b) (symbol<? a b))))))))
+              (sort (hash-table-ref/default *tests* group '())
+                    (lambda (a b) (string<? a b)))))
+          (sort *groups*
+                (lambda (a b) (symbol<? a b))))
         (format #t "</tbody></table></body></html>")))))
 
 (format-stats)
